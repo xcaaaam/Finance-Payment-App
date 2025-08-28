006807f7b092f2b4385290a4061e6f63496737d4
xcaaaam <camisolano71000@gmail.com>
2025-08-28 16:41:38 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Tailwind
+import App from "./App";      // <-- Importa App desde el mismo folder
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
